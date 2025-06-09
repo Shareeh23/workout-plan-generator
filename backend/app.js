f67e0ac57f28c558e64319a85057d9d38e20d8e1
shareeh06@gmail.com <shareeh06@gmail.com>
2025-06-09 17:30:38 +0530

    moved passport setup to util folder and added JWT auth middleware

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-require('./util/passport')
+require('./config/passport')
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
